a748d2b38f709c4d4fca30e472561aa2171f82ad
shiuanyulo <edcrfvlily5@gmail.com>
2020-11-04 16:47:31 +0800

    修改主頁樣式

--- RCM_MVVM/First_MVVM/ViewModels/MainWindowViewModel.cs
+++ RCM_MVVM/First_MVVM/ViewModels/MainWindowViewModel.cs
@@ -19,23 +19,6 @@ namespace First_MVVM.ViewModels
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
-
-        private string _userName;
-
-        public string UserName
-        {
-            get { return _userName; }
-            set { _userName = value; }
-        }
-
-        private string _passWord;
-
-        public string PassWord
-        {
-            get { return _passWord; }
-            set { _passWord = value; }
-        }
-
         private string _updateText;
         public string UpdateText
         {
@@ -43,15 +26,10 @@ namespace First_MVVM.ViewModels
             set { SetProperty(ref _updateText, value); }
         }
 
-
-        public DelegateCommand<object> LoginCommand { get; private set; }
-
-        public InteractionRequest<ICustomNotification> RegisterAccountViewRequest { get; set; }
         public InteractionRequest<ICustomNotification> RegisterStepTabRequest { get; set; }
         public InteractionRequest<ICustomNotification> CheckOutStepTabRequest { get; set; }
         public InteractionRequest<ICustomNotification> CheckInStepTabRequest { get; set; }
 
-        public DelegateCommand RegisterAccountViewCommand { get; set; }
         public DelegateCommand RegisterStepTabCommand { get; set; }
         public DelegateCommand CheckOutStepTabCommand { get; set; }
         public DelegateCommand CheckInStepTabCommand { get; set; }
@@ -59,45 +37,12 @@ namespace First_MVVM.ViewModels
         public MainWindowViewModel()
         {
             IO.SetDevicePort("COM3", 57600); IO.SetIOParameter();
-            LoginCommand = new DelegateCommand<object>(LoginExecute);
-            RegisterAccountViewRequest = new InteractionRequest<ICustomNotification>();
-            RegisterAccountViewCommand = new DelegateCommand(RaiseRegisterAccountView);
             RegisterStepTabRequest = new InteractionRequest<ICustomNotification>();
             RegisterStepTabCommand = new DelegateCommand(RegisterStepTabView);
             CheckOutStepTabRequest = new InteractionRequest<ICustomNotification>();
             CheckOutStepTabCommand = new DelegateCommand(CheckOutStepTabView);
             CheckInStepTabRequest = new InteractionRequest<ICustomNotification>();
             CheckInStepTabCommand = new DelegateCommand(CheckInStepTabView);
-        }
-
-        private void LoginExecute(object parameter) 
-        {
-            var passwordBox = parameter as PasswordBox;
-
-            if (passwordBox != null) { PassWord = passwordBox.Password; }
-
-            if (!String.IsNullOrWhiteSpace(UserName) && !String.IsNullOrWhiteSpace(PassWord))
-            { 
-                string password = "123456";
-
-                if (password == PassWord)
-                {
-                    UpdateText = "登入成功";
-                }
-                else
-                {
-                    UpdateText = "登入失敗";
-                }
-            }
-            else
-            {
-                UpdateText = "帳號密碼不可為空白";
-            }
-        }
-
-        private void RaiseRegisterAccountView()
-        {
-            RegisterAccountViewRequest.Raise(new CustomNotification { Title = "Register Account" });
         }
 
         private void RegisterStepTabView()
