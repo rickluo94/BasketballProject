cb233ffff170b8337939ae8ff8c2b24747cc3ce9
shiuanyulo <edcrfvlily5@gmail.com>
2020-11-16 17:59:40 +0800

    新增等待畫面

---
+++ RCM_MVVM/First_MVVM/Views/LoadingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace First_MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for LoadingPage
+    /// </summary>
+    public partial class LoadingPage : UserControl
+    {
+        public LoadingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
