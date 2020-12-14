0ac158b5a4400fd21849bd83f5386b1b4db77682
shiuanyulo <edcrfvlily5@gmail.com>
2020-12-14 17:31:19 +0800

    服務 =>加入新變數SN

--- RCM_MVVM/First_MVVM/Models/MemberServiceModel.cs
+++ RCM_MVVM/First_MVVM/Models/MemberServiceModel.cs
@@ -6,6 +6,7 @@ namespace First_MVVM.Models
 {
     public class MemberServiceModel
     {
+        public string SN { get; set; }
         public string ID { get; set; }
         public string CardID { get; set; }
         public string Balance { get; set; }
