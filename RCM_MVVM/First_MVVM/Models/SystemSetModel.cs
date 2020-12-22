0c46655275acdd0578f7aa40cc654c45fdc74799
shiuanyulo <edcrfvlily5@gmail.com>
2020-12-22 11:26:51 +0800

    新增系統設定模型

---
+++ RCM_MVVM/First_MVVM/Models/SystemSetModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First_MVVM.Models
+{
+    public class SystemSetModel
+    {
+        public static string StationName { get; set; }
+    }
+}
