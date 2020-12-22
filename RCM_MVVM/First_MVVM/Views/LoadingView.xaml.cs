ce7da97a5c1a72413dbaef0a230c85cba3115898
shiuanyulo <edcrfvlily5@gmail.com>
2020-12-22 11:56:29 +0800

    畫面載入等待畫面

---
+++ RCM_MVVM/First_MVVM/Views/LoadingView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace First_MVVM.Views
+{
+    /// <summary>
+    /// LoadingView.xaml 的互動邏輯
+    /// </summary>
+    public partial class LoadingView : Window
+    {
+        public LoadingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
